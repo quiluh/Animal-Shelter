734abcf4253025df3933f62a20e2731c201d80bb
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-06-09 12:09:33 +1200

    established connection

--- app.py
+++ app.py
@@ -2,4 +2,12 @@ import pymysql
 from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
+def create_connection():
+    return pymysql.connect(
+        host="10.0.0.17:3306",
+        user="clymesa",
+        password="ANGLE",
+        db="animal-shelter"
+    )
+
 app.run(debug=True)
