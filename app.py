e0c13ec31a1d00cde709f693643d2e51841e3cd5
Clyde Mesa <95511845+quiluh@users.noreply.github.com>
2025-06-16 12:16:55 +1200

    generated random dogs for column and rows

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import pymysql
 from flask import Flask, render_template, request, redirect
+import random
+
 app = Flask(__name__)
 
 def create_connection():
@@ -13,10 +15,24 @@ def create_connection():
 
 @app.route("/")
 def Home():
+    randomIndexes = []
+    rows,columns = (1,3)
+    randomDogs = [[None for i in range(columns)] for o in range(rows)]
+    
     with create_connection() as connection:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * from dogs")
-            result = cursor.fetchall()
-    return render_template("index.html",allDogs=result)
+            result = list(cursor.fetchall())
+            while len(randomIndexes) != rows*columns:
+                index = random.randint(0,len(result)-1)
+                if index not in randomIndexes:
+                    randomIndexes.append(index)
+
+    randomIndexes = iter(randomIndexes)
+    for i in randomDogs:
+        for index in range(len(i)):
+            i[index] = result[next(randomIndexes)]
+
+    return render_template("index.html",allDogs=randomDogs)
 
 app.run(debug=True)
